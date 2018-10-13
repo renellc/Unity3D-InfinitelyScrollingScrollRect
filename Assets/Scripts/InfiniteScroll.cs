660258422e7964f09b7179c2ad80369a5136fa8b
Renell Castro (rmc424) <rmc424@mail.usask.ca>
2018-10-13 01:27:21 -0600

    Initial commit

---
+++ Assets/Scripts/InfiniteScroll.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InfiniteScroll : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    [SerializeField]
+    protected ScrollContent scrollContent;
+    [SerializeField]
+    protected float outOfBoundsThreshold;
+
+    private ScrollRect scrollRect;
+    private Vector2 lastTouchPosition;
+    private bool positiveDrag;
+    
+
+    private void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        scrollRect.vertical = scrollContent.Vertical;
+        scrollRect.horizontal = scrollContent.Horizontal;
+        scrollRect.movementType = ScrollRect.MovementType.Unrestricted;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        lastTouchPosition = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (scrollRect.vertical)
+            positiveDrag = lastTouchPosition.y > eventData.position.y;
+        else
+            positiveDrag = lastTouchPosition.x > eventData.position.x;
+
+        lastTouchPosition = eventData.position;
+    }
+
+    public void OnViewScroll()
+    {
+        if (scrollRect.vertical)
+            HandleVerticalScroll();
+        else
+            HandleHorizontalScroll();
+    }
+
+    private void HandleVerticalScroll()
+    {
+
+    }
+
+    private void HandleHorizontalScroll()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ScrollContent.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+public class ScrollContent : MonoBehaviour
+{
+    #region Public Properties
+
+    /// <summary>
+    /// How far apart each item is in the scroll view.
+    /// </summary>
+    public float ItemSpacing { get { return itemSpacing; } }
+
+    /// <summary>
+    /// How much the items are indented from left and right of the scroll view.
+    /// </summary>
+    public float HorizontalMargin { get { return horizontalMargin; } }
+
+    /// <summary>
+    /// How much the items are indented from top and bottom of the scroll view.
+    /// </summary>
+    public float VerticalMargin { get { return verticalMargin; } }
+
+    /// <summary>
+    /// Is the scroll view oriented horizontally?
+    /// </summary>
+    public bool Horizontal { get { return horizontal; } }
+
+    /// <summary>
+    /// Is the scroll view oriented vertically?
+    /// </summary>
+    public bool Vertical { get { return vertical; } }
+
+    #endregion
+
+    #region Protected Members
+
+    /// <summary>
+    /// How far apart each item is in the scroll view.
+    /// </summary>
+    [SerializeField]
+    protected float itemSpacing;
+
+    /// <summary>
+    /// How much the items are indented from the top/bottom and left/right of the scroll view.
+    /// </summary>
+    [SerializeField]
+    protected float horizontalMargin, verticalMargin;
+
+    /// <summary>
+    /// Is the scroll view oriented horizontall or vertically?
+    /// </summary>
+    [SerializeField]
+    protected bool horizontal, vertical;
+
+    #endregion
+
+    #region Private Members
+
+    /// <summary>
+    /// The RectTransform component of the scroll content.
+    /// </summary>
+    private RectTransform rectTransform;
+
+    /// <summary>
+    /// The RectTransform components of all the children of this GameObject.
+    /// </summary>
+    private RectTransform[] rtChildren;
+
+    /// <summary>
+    /// The width and height of the parent.
+    /// </summary>
+    private float parentWidth, parentHeight;
+
+    /// <summary>
+    /// The width and height of the children GameObjects.
+    /// </summary>
+    private float childWidth, childHeight;
+
+    #endregion
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        rtChildren = new RectTransform[rectTransform.childCount];
+
+        for (int i = 0; i < rectTransform.childCount; i++)
+        {
+            rtChildren[i] = rectTransform.GetChild(i) as RectTransform;
+        }
+
+        // Subtract the margin from both sides.
+        parentWidth = rectTransform.rect.width - (2 * horizontalMargin);
+
+        // Subtract the margin from the top and bottom.
+        parentHeight = rectTransform.rect.height - (2 * verticalMargin);
+
+        childWidth = rtChildren[0].rect.width;
+        childHeight = rtChildren[0].rect.height;
+
+        horizontal = !vertical;
+        if (vertical)
+            InitializeContentVertical();
+        else
+            InitializeContentHorizontal();
+    }
+
+    /// <summary>
+    /// Initializes the scroll content if the scroll view is oriented horizontally.
+    /// </summary>
+    private void InitializeContentHorizontal()
+    {
+        float originX = 0 - (parentWidth * 0.5f);
+        float posOffset = childWidth * 0.5f;
+        for (int i = 0; i < rtChildren.Length; i++)
+        {
+            Vector2 childPos = rtChildren[i].localPosition;
+            childPos.x = originX + posOffset + i * (childWidth + itemSpacing);
+            rtChildren[i].localPosition = childPos;
+
+            // If the child appears outside of the viewable area, disable it.
+            if (rtChildren[i].localPosition.x - posOffset > 0 + parentWidth * 0.5f)
+            {
+                rtChildren[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes the scroll content if the scroll view is oriented vertically.
+    /// </summary>
+    private void InitializeContentVertical()
+    {
+        float originY = 0 - (parentHeight * 0.5f);
+        float posOffset = childHeight * 0.5f;
+        for (int i = 0; i < rtChildren.Length; i++)
+        {
+            Vector2 childPos = rtChildren[i].localPosition;
+            childPos.x = originY + posOffset + i * (childHeight + itemSpacing);
+            rtChildren[i].localPosition = childPos;
+
+            // If the child appears outside of the viewable area, disable it.
+            if (rtChildren[i].localPosition.y - posOffset > 0 + parentHeight * 0.5f)
+            {
+                rtChildren[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
